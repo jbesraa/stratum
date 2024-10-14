fe6aaa43a1e30572625cca8ccc7f6faa2f151424
jbesraa <jbesraa@gmail.com>
2024-10-14 14:49:38 +0300

    Test `JDC` fallback functionality after submit..
    share is rejected by pool

--- roles/tests-integration/tests/pool_integration.rs
+++ roles/tests-integration/tests/pool_integration.rs
@@ -1,5 +1,7 @@
 mod common;
 
+use std::str::FromStr;
+
 use roles_logic_sv2::{
     common_messages_sv2::{Protocol, SetupConnection},
     parsers::{CommonMessages, PoolMessages, TemplateDistribution},
@@ -37,4 +39,31 @@ async fn success_pool_template_provider_connection() {
     assert_tp_message!(&sniffer.next_downstream_message(), CoinbaseOutputDataSize);
     assert_tp_message!(&sniffer.next_upstream_message(), NewTemplate);
     assert_tp_message!(sniffer.next_upstream_message(), SetNewPrevHash);
+}
+
+// "This test does",
+// "Launch a pool mocks",
+// "Launch jds",
+// "Launch jdc",
+// "Launch mining-proxy",
+// "Launch a mining device",
+// "Act like a pool",
+// "Connect jdc to the first pool",
+// "Receive an submit share error from first pool",
+// "Close jdc connection with first pool",
+// "Connect jdc the pool mocked by this test"
+#[tokio::test]
+#[ignore]
+async fn test_jdc_fallback_after_submit_share_error() {
+    let tp_addr = common::get_available_address();
+    let pool_addr = common::get_available_address();
+    let _tp = common::start_template_provider(tp_addr.port()).await;
+    let _ = common::start_pool(Some(pool_addr), Some(tp_addr)).await;
+    let jds_addr = common::start_jds(tp_addr).await;
+    common::start_jdc(
+        pool_addr,
+        tp_addr,
+        std::net::SocketAddr::from_str(&jds_addr).unwrap(),
+    )
+    .await;
 }
