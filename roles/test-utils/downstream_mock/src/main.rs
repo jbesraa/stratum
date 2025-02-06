03c426f6586f38b90661437fa5d4d3e34b12d54f
jbesraa <jbesraa@gmail.com>
2025-02-06 16:45:35 +0200

    working handshake+setup connection

--- roles/test-utils/downstream_mock/src/main.rs
+++ roles/test-utils/downstream_mock/src/main.rs
@@ -1,25 +1,37 @@
-use std::error::Error;
-use std::net::SocketAddr;
-use std::sync::{Arc, RwLock};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpStream;
-use tokio::sync::mpsc;
-use tokio::time::sleep;
+use codec_sv2::{
+    HandShakeFrame, HandshakeRole, StandardEitherFrame, StandardNoiseDecoder, Sv2Frame,
+};
+use const_sv2::{INITIATOR_EXPECTED_HANDSHAKE_MESSAGE_SIZE, MESSAGE_TYPE_SETUP_CONNECTION};
+use noise_sv2::Initiator;
+use roles_logic_sv2::{
+    common_messages_sv2::{Protocol, SetupConnection},
+    parsers::{AnyMessage, CommonMessages, PoolMessages},
+};
+use std::{error::Error, net::SocketAddr, sync::Arc, thread::sleep, time::Duration};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+    sync::mpsc,
+};
 
 #[derive(Debug)]
 pub struct DownstreamRole {
-    addr: SocketAddr,
-    sender_outgoing_data: Option<mpsc::Sender<Vec<u8>>>,
-    runtime: Arc<RwLock<Option<Arc<tokio::runtime::Runtime>>>>,
+    upstream_addr: SocketAddr,
+    sender_outgoing_data: Option<mpsc::Sender<StandardEitherFrame<AnyMessage<'static>>>>,
+    state: Arc<tokio::sync::RwLock<codec_sv2::State>>,
+    runtime: Arc<std::sync::RwLock<Option<Arc<tokio::runtime::Runtime>>>>,
     stop_me: tokio::sync::watch::Sender<()>,
 }
 
 impl DownstreamRole {
-    pub fn new(addr: SocketAddr) -> Self {
+    pub fn new(upstream_addr: SocketAddr) -> Self {
         let (stop_me, _) = tokio::sync::watch::channel(());
         Self {
-            runtime: Arc::new(RwLock::new(None)),
-            addr,
+            runtime: Arc::new(std::sync::RwLock::new(None)),
+            upstream_addr,
+            state: Arc::new(tokio::sync::RwLock::new(codec_sv2::State::not_initialized(
+                &HandshakeRole::Initiator(Initiator::without_pk().unwrap()),
+            ))),
             sender_outgoing_data: None,
             stop_me,
         }
@@ -29,91 +41,110 @@ impl DownstreamRole {
         let runtime = tokio::runtime::Builder::new_multi_thread()
             .enable_all()
             .build()?;
+        // Channel for receiving data from the upstream. `sender_incoming_data` is used to send
+        // data to the channel and `receiver_incoming_data` is continuously listening for data to
+        // receive in an async loop.
+        let (sender_incoming_data, mut receiver_outgoing_data) =
+            tokio::sync::mpsc::channel::<StandardEitherFrame<AnyMessage<'static>>>(1024);
+        // Channel for sending data to the upstream. `sender_outgoing_data` is used to send data to
+        // to the channel and `receiver_outgoing_data` is continuously listening for data to send
+        // in an async loop.
+        let (sender_outgoing_data, mut receiver_incoming_data) =
+            mpsc::channel::<StandardEitherFrame<AnyMessage<'static>>>(1024);
 
-        let (sender_incoming_data, mut receiver_incoming_data) = mpsc::channel(1024);
-        let (sender_outgoing_data, mut receiver_outgoing_data) = mpsc::channel(1024);
         self.sender_outgoing_data = Some(sender_outgoing_data.clone());
 
+        // Acquire the lock and check if the runtime is already set.
         let mut self_runtime = self.runtime.write().unwrap();
         if self_runtime.is_some() {
             return Err("Runtime already set".into());
         }
-
-        let addr = self.addr.clone();
-        let mut stop_listening = self.stop_me.subscribe();
+        let upstream_addr = self.upstream_addr.clone();
         let stream = tokio::task::block_in_place(|| {
-            runtime.block_on(async { TcpStream::connect(addr.clone()).await.unwrap() })
+            runtime.block_on(async { TcpStream::connect(upstream_addr).await.unwrap() })
         });
+
+        // Split the stream into reader and writer.
         let (mut reader, mut writer) = stream.into_split();
+
+        let original_state = self.state.clone();
+        let original_state_1 = self.state.clone();
+        let original_state_2 = self.state.clone();
+        let mut stop_reader_writer = self.stop_me.subscribe();
         runtime.spawn(async move {
+            let mut decoder = StandardNoiseDecoder::<AnyMessage<'static>>::new();
+            let mut encoder = codec_sv2::NoiseEncoder::<AnyMessage<'static>>::new();
             loop {
                 tokio::select!(
-                    _ = stop_listening.changed() => {
-                        println!("Stopping listener");
-                        return;
-                    },
-                    _ = async {
-                        if let Some(data) = receiver_outgoing_data.recv().await {
-                            if let Err(e) = writer.write_all(&data).await {
-                                eprintln!("Error sending data: {}", e);
-                            }
-                            println!("Sent data to upstream: {:?}", String::from_utf8_lossy(&data));
-                        }
-                    } => {}
+                  _ = stop_reader_writer.changed() => {
+                    break;
+                  }
+                  _ = async {
+                      loop {
+                          if let Some(m) = receiver_incoming_data.recv().await{
+                          dbg!("here");
+                          dbg!(&m);
+                          let state = original_state.clone();
+                          let mut state = state.write().await;
+                          dbg!(&state);
+                          let b = encoder.encode(m.into(), &mut state).unwrap();
+                          let b = b.as_ref();
+                          let _ = writer.write_all(b).await;
+                          dbg!("Sent data");
+                      }
+                      }
+                  } => {}
+                  _ = async {
+                      loop
+                      {
+                          let writable = decoder.writable();
+                          if let Ok(_) =  reader.read_exact(writable).await {
+                              let state = original_state_1.clone();
+                              let mut state = state.write().await;
+                              match decoder.next_frame(&mut state) {
+                                  Ok(x) => {
+                                      let _ = sender_incoming_data.send(x.into()).await;
+                                  },
+                                  Err(e) => {
+                                      dbg!("error: {}", e);
+                                  }
+                              };
+                          }
+                      }                  } => {}
                 );
             }
         });
 
-        let mut stop_reader = self.stop_me.subscribe();
+        // Perform Handshake
         runtime.spawn(async move {
-            loop {
-                tokio::select!(
-                    _ = stop_reader.changed() => {
-                        println!("Stopping listener");
-                        return;
-                    },
-                    _ = async {
-                        let mut buf = Vec::<u8>::new();
-                        let _ = reader.readable().await;
-                        if let Err(e) = reader.read_to_end(&mut buf).await {
-                            dbg!("Error reading response: {}", e);
-                        } else {
-                            if !buf.is_empty() {
-                                dbg!("Received response: {}", String::from_utf8_lossy(&buf));
-                                sender_incoming_data.send(buf).await.unwrap();
-                            }
-                        }
-                        sleep(std::time::Duration::from_secs(3)).await;
-                    } => {}
-                );
-            }
-        });
-
-        let mut stop_receiver = self.stop_me.subscribe();
-        // let stop_rt = self.stop_me.clone();
-        runtime.spawn(async move {
-            tokio::select!(
-                _ = stop_receiver.changed() => {
-                    println!("Stopping receiver");
-                    return;
-                },
-                _ = async {
-                    while let Some(data) = receiver_incoming_data.recv().await {
-                        println!(
-                            "Received data from downstream: {:?}",
-                            String::from_utf8_lossy(&data)
-                        );
-                        // let a = stop_rt.send(());
-                    }
-                } => {}
-            );
+            let role = HandshakeRole::Initiator(Initiator::without_pk().unwrap());
+            let mut initialized_state = codec_sv2::State::initialized(role);
+            let first_message = initialized_state.step_0().unwrap();
+            sender_outgoing_data
+                .send(first_message.into())
+                .await
+                .unwrap();
+            let decoded = receiver_outgoing_data.recv().await.unwrap();
+            let second_message: HandShakeFrame = decoded.try_into().unwrap();
+            let second_message: [u8; INITIATOR_EXPECTED_HANDSHAKE_MESSAGE_SIZE] = second_message
+                .get_payload_when_handshaking()
+                .try_into()
+                .unwrap();
+            let transport_mode = initialized_state.step_2(second_message).unwrap();
+            let mut ori_state = original_state_2.write().await;
+            sleep(Duration::from_secs(3));
+            *ori_state = transport_mode;
+            dbg!(&ori_state);
         });
 
         *self_runtime = Some(Arc::new(runtime));
         Ok(())
     }
 
-    pub fn send_data(&self, data: Vec<u8>) -> Result<(), Box<dyn Error>> {
+    pub fn send_data(
+        &self,
+        data: StandardEitherFrame<AnyMessage<'static>>,
+    ) -> Result<(), Box<dyn Error>> {
         if let Some(sender) = self.sender_outgoing_data.as_ref() {
             self.runtime
                 .read()
@@ -128,20 +159,61 @@ impl DownstreamRole {
         }
         Ok(())
     }
+
+    pub fn shutdown(&self) {
+        self.stop_me.send(()).unwrap();
+    }
+}
+
+fn get_setup_connection_message(
+    min_version: u16,
+    max_version: u16,
+    is_work_selection_enabled: bool,
+    protocol: Protocol,
+) -> SetupConnection<'static> {
+    let endpoint_host = "0.0.0.0".to_string().into_bytes().try_into().unwrap();
+    let vendor = String::new().try_into().unwrap();
+    let hardware_version = String::new().try_into().unwrap();
+    let firmware = String::new().try_into().unwrap();
+    let device_id = String::new().try_into().unwrap();
+    let flags = match is_work_selection_enabled {
+        false => 0b0000_0000_0000_0000_0000_0000_0000_0100,
+        true => 0b0000_0000_0000_0000_0000_0000_0000_0110,
+    };
+    SetupConnection {
+        protocol,
+        min_version,
+        max_version,
+        flags,
+        endpoint_host,
+        endpoint_port: 50,
+        vendor,
+        hardware_version,
+        firmware,
+        device_id,
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let addr: SocketAddr = "127.0.0.1:8080".parse()?;
-    let mut upstream_role = DownstreamRole::new(addr);
-    upstream_role.start()?; // Start the upstream role
+    let upstream_addr: SocketAddr = "127.0.0.1:8080".parse()?;
+    let mut downstream_role = DownstreamRole::new(upstream_addr);
+    downstream_role.start()?;
 
-    upstream_role.send_data("Hello from downstream".as_bytes().to_vec())?;
+    sleep(Duration::from_secs(2));
+    let setup_connection_message =
+        get_setup_connection_message(2, 2, false, Protocol::JobDeclarationProtocol);
+    let any_message =
+        PoolMessages::Common(CommonMessages::SetupConnection(setup_connection_message));
+    let sv2_frame =
+        Sv2Frame::from_message(any_message, MESSAGE_TYPE_SETUP_CONNECTION, 0, false).unwrap();
+    let frame = StandardEitherFrame::<AnyMessage>::Sv2(sv2_frame);
+    downstream_role.send_data(frame)?;
 
-    let runtime = upstream_role.runtime.read().unwrap().clone().unwrap();
+    let runtime = downstream_role.runtime.read().unwrap().clone().unwrap();
     runtime.block_on(async {
         loop {
             tokio::time::sleep(std::time::Duration::from_millis(100)).await;
-            if upstream_role
+            if downstream_role
                 .sender_outgoing_data
                 .as_ref()
                 .map_or(true, |s| s.is_closed())
--- roles/test-utils/upstream_mock/src/main.rs
+++ roles/test-utils/upstream_mock/src/main.rs
@@ -1,123 +1,189 @@
-use std::error::Error;
-use std::net::SocketAddr;
-use std::sync::{Arc, RwLock};
-use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use tokio::net::TcpListener;
-use tokio::sync::mpsc;
+use binary_sv2::{Deserialize, GetSize, Serialize};
+use codec_sv2::{HandShakeFrame, HandshakeRole, StandardEitherFrame, StandardNoiseDecoder};
+use const_sv2::RESPONDER_EXPECTED_HANDSHAKE_MESSAGE_SIZE;
+use key_utils::{Secp256k1PublicKey, Secp256k1SecretKey};
+use noise_sv2::Responder;
+use roles_logic_sv2::parsers::AnyMessage;
+use std::{
+    error::Error,
+    net::SocketAddr,
+    sync::{Arc, RwLock},
+    time::Duration,
+};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpListener,
+    sync::mpsc,
+    time::sleep,
+};
 
 #[derive(Debug)]
 pub struct UpstreamRole {
-    addr: SocketAddr,
-    sender: Option<mpsc::Sender<Vec<u8>>>,
+    upstream_addr: SocketAddr,
+    state: Arc<tokio::sync::RwLock<codec_sv2::State>>,
     runtime: Arc<RwLock<Option<Arc<tokio::runtime::Runtime>>>>,
     stop_me: tokio::sync::watch::Sender<()>,
 }
 
 impl UpstreamRole {
-    pub fn new(addr: SocketAddr) -> Self {
+    pub fn new(upstream_addr: SocketAddr) -> Self {
         let (stop_me, _) = tokio::sync::watch::channel(());
+        let pub_key = "9auqWEzQDVyd2oe1JVGFLMLHZtCo2FFqZwtKA5gd9xbuEu7PH72"
+            .to_string()
+            .parse::<Secp256k1PublicKey>()
+            .unwrap()
+            .into_bytes();
+        let prv_key = "mkDLTBBRxdBv998612qipDYoTK3YUrqLe8uWw7gu3iXbSrn2n"
+            .to_string()
+            .parse::<Secp256k1SecretKey>()
+            .unwrap()
+            .into_bytes();
+        let responder =
+            Responder::from_authority_kp(&pub_key, &prv_key, std::time::Duration::from_secs(10000))
+                .unwrap();
         Self {
+            state: Arc::new(tokio::sync::RwLock::new(codec_sv2::State::not_initialized(
+                &HandshakeRole::Responder(responder),
+            ))),
             runtime: Arc::new(RwLock::new(None)),
-            addr,
-            sender: None,
+            upstream_addr,
             stop_me,
         }
     }
 
-    pub fn start(&mut self) -> Result<(), Box<dyn Error>> {
+    pub fn start<'a, Message: Serialize + Deserialize<'a> + GetSize + Send + 'static>(
+        &mut self,
+    ) -> Result<(), Box<dyn Error>> {
         let runtime = tokio::runtime::Builder::new_multi_thread()
             .enable_all()
             .build()?;
 
-        let (send, mut receive) = mpsc::channel(1024);
-        self.sender = Some(send.clone());
+        let (sender_outgoing_data, mut receiver_incoming_data) =
+            mpsc::channel::<StandardEitherFrame<AnyMessage<'static>>>(1024);
 
+        let (sender_incoming_data, mut receiver_outgoing_data) =
+            mpsc::channel::<StandardEitherFrame<AnyMessage<'static>>>(1024);
+
+        // Acquire the lock and check if the runtime is already set.
         let mut self_runtime = self.runtime.write().unwrap();
         if self_runtime.is_some() {
             return Err("Runtime already set".into());
         }
-
-        let cloned_tx = send.clone();
-        let addr = self.addr.clone();
-
-        let mut stop_listening = self.stop_me.subscribe();
+        let upstream_addr = self.upstream_addr.clone();
+        let tcp = tokio::task::block_in_place(|| {
+            runtime.block_on(async { TcpListener::bind(upstream_addr.clone()).await.unwrap() })
+        });
+        println!("Upstream accepting connections on: {}", upstream_addr);
+        let original_state = self.state.clone();
+        let original_state_1 = self.state.clone();
+        let original_state_2 = self.state.clone();
+        let mut stop_reader_writer = self.stop_me.subscribe();
+        let sender_outgoing_data = sender_outgoing_data.clone();
         runtime.spawn(async move {
-            let listener = TcpListener::bind(addr.clone()).await.unwrap();
-            println!("UpstreamRole listening on {}", addr);
             loop {
+                let (stream, _addr) = tcp.accept().await.unwrap();
+                let (mut reader, mut writer) = stream.into_split();
+                let mut decoder = StandardNoiseDecoder::<AnyMessage<'static>>::new();
+                let mut encoder = codec_sv2::NoiseEncoder::<AnyMessage<'static>>::new();
                 tokio::select!(
-                    _ = stop_listening.changed() => {
-                        println!("Stopping listener");
-                        return;
-                    },
-                    _ = async {
-                        let (stream, addr) = listener.accept().await.unwrap();
-                        let (mut reader, mut writer) = stream.into_split();
-                        println!("Accepted connection from {}", addr);
-                        let mut buf = vec![0; 1024];
-                        let n = reader.read(&mut buf).await.unwrap();
-                        let data = &buf[..n];
-
-                        if let Err(e) = cloned_tx.send(data.to_vec()).await {
-                            println!("Error sending data: {}", e);
-                        } else {
-                            println!("Data sent");
+                _ = stop_reader_writer.changed() => { }
+                _ = async {
+                  loop {
+                    if let Some(m) = receiver_incoming_data.recv().await{
+                    let state = original_state.clone();
+                    let mut state = state.write().await;
+                    dbg!(&state);
+                    dbg!(&m);
+                    let b = encoder.encode(m.into(), &mut state).unwrap();
+                    let b = b.as_ref();
+                    let _ = writer.write_all(b).await;
+                    dbg!("Sent data");
+                  }
+                  }
+				} => {}
+                  _ = async {
+                  loop {
+                    let writable = decoder.writable();
+                    if let Ok(_) =  reader.read_exact(writable).await {
+                      let state = original_state_1.clone();
+                      let mut state = state.write().await;
+                      dbg!(&state);
+                      match decoder.next_frame(&mut state) {
+                        Ok(x) => {
+                          dbg!(&x);
+                          let _ = sender_incoming_data.send(x.into()).await;
+                        },
+                        Err(e) => {
+                          dbg!("error: {}", e);
                         }
-                        let d = "Hello from upstream".as_bytes();
-                        let res = writer.write_all(d).await.unwrap();
-                        dbg!(&res);
-                    } => {}
+                      };
+                    }
+                  }
+                } => {}
                 );
             }
         });
-
-        let mut stop_receiver = self.stop_me.subscribe();
-        // let stop_rt = self.stop_me.clone();
         runtime.spawn(async move {
-            loop {
-                tokio::select!(
-                    _ = stop_receiver.changed() => {
-                        println!("Stopping receiver");
-                        return;
-                    },
-                    _ = async {
-                        while let Some(data) = receive.recv().await {
-                            println!(
-                                "Received data from downstream: {:?}",
-                                String::from_utf8_lossy(&data)
-                            );
-                            // let a = stop_rt.send(());
-                        }
-                    } => {}
-                );
-            }
+            let responder = Responder::from_authority_kp(
+                &"9auqWEzQDVyd2oe1JVGFLMLHZtCo2FFqZwtKA5gd9xbuEu7PH72"
+                    .to_string()
+                    .parse::<Secp256k1PublicKey>()
+                    .unwrap()
+                    .into_bytes(),
+                &"mkDLTBBRxdBv998612qipDYoTK3YUrqLe8uWw7gu3iXbSrn2n"
+                    .to_string()
+                    .parse::<Secp256k1SecretKey>()
+                    .unwrap()
+                    .into_bytes(),
+                std::time::Duration::from_secs(10000),
+            )
+            .unwrap();
+            let role = HandshakeRole::Responder(responder);
+            let mut initial_state = codec_sv2::State::initialized(role);
+            dbg!("Initialized state");
+            let decoded = receiver_outgoing_data.recv().await.unwrap();
+            let first_message: HandShakeFrame = decoded.try_into().unwrap();
+            dbg!("Received first message");
+            let first_message: [u8; RESPONDER_EXPECTED_HANDSHAKE_MESSAGE_SIZE] = first_message
+                .get_payload_when_handshaking()
+                .try_into()
+                .unwrap();
+
+            dbg!("Before sending second message");
+            // Create and send second handshake message
+            let (second_message, transport_mode) = initial_state.step_1(first_message).unwrap();
+
+            let res = sender_outgoing_data
+                .send(second_message.into())
+                .await
+                .unwrap();
+            dbg!(&res);
+            dbg!("After sending second message");
+
+            sleep(Duration::from_secs(3)).await;
+            let mut ori_state = original_state_2.write().await;
+            *ori_state = transport_mode;
         });
 
         *self_runtime = Some(Arc::new(runtime));
         Ok(())
     }
+
+    pub fn shutdown(&self) {
+        self.stop_me.send(()).unwrap();
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let addr: SocketAddr = "127.0.0.1:8080".parse()?;
-    let mut upstream_role = UpstreamRole::new(addr);
-    upstream_role.start()?; // Start the upstream role
+    let upstream_addr: SocketAddr = "127.0.0.1:8080".parse()?;
+    let mut upstream_role = UpstreamRole::new(upstream_addr);
+    upstream_role.start::<AnyMessage>()?; // Start the upstream role
 
-    let runtime = upstream_role.runtime.read().unwrap().clone().unwrap(); // Get the runtime
-
-    // Block the main thread using runtime.block_on
+    let runtime = upstream_role.runtime.read().unwrap().clone().unwrap();
     runtime.block_on(async {
         loop {
-            tokio::time::sleep(std::time::Duration::from_millis(100)).await; // Small delay to avoid busy-looping
-            if upstream_role
-                .sender
-                .as_ref()
-                .map_or(true, |s| s.is_closed())
-            {
-                break; // Exit the loop when the sender is closed
-            }
+            tokio::time::sleep(std::time::Duration::from_millis(100)).await;
         }
-        println!("Main task done."); // This will print when the sender is dropped.
     });
 
     Ok(())
