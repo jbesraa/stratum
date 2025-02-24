1c223ea9e0b2f97deeff740f9bd772c044c6bf27
jbesraa <jbesraa@gmail.com>
2025-02-24 13:44:01 +0200

    Rename `Configuration` to `JobDeclaratorServerConfig`

--- roles/jd-server/src/lib/config.rs
+++ roles/jd-server/src/lib/config.rs
@@ -4,7 +4,7 @@ use serde::Deserialize;
 use std::{convert::TryFrom, time::Duration};
 
 #[derive(Debug, serde::Deserialize, Clone)]
-pub struct Configuration {
+pub struct JobDeclaratorServerConfig {
     #[serde(default = "default_true")]
     pub async_mining_allowed: bool,
     pub listen_jd_address: String,
@@ -18,6 +18,32 @@ pub struct Configuration {
     pub core_rpc_pass: String,
     #[serde(deserialize_with = "duration_from_toml")]
     pub mempool_update_interval: Duration,
+}
+
+impl JobDeclaratorServerConfig {
+    pub fn new(
+        listen_jd_address: String,
+        authority_public_key: Secp256k1PublicKey,
+        authority_secret_key: Secp256k1SecretKey,
+        cert_validity_sec: u64,
+        coinbase_outputs: Vec<CoinbaseOutput>,
+        core_rpc: CoreRpc,
+        mempool_update_interval: Duration,
+    ) -> Self {
+        Self {
+            async_mining_allowed: true,
+            listen_jd_address,
+            authority_public_key,
+            authority_secret_key,
+            cert_validity_sec,
+            coinbase_outputs,
+            core_rpc_url: core_rpc.url,
+            core_rpc_port: core_rpc.port,
+            core_rpc_user: core_rpc.user,
+            core_rpc_pass: core_rpc.pass,
+            mempool_update_interval,
+        }
+    }
 }
 
 fn default_true() -> bool {
@@ -78,6 +104,25 @@ impl TryFrom<&CoinbaseOutput> for CoinbaseOutput_ {
                 output_script_value: pool_output.clone().output_script_value,
             }),
             _ => Err(roles_logic_sv2::errors::Error::UnknownOutputScriptType),
+        }
+    }
+}
+
+#[derive(Debug, Deserialize, Clone)]
+pub struct CoreRpc {
+    url: String,
+    port: u16,
+    user: String,
+    pass: String,
+}
+
+impl CoreRpc {
+    pub fn new(url: String, port: u16, user: String, pass: String) -> Self {
+        Self {
+            url,
+            port,
+            user,
+            pass,
         }
     }
 }
