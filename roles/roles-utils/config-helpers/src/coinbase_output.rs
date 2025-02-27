d894892edc5fc56c9991be23c26032965b9109b2
jbesraa <jbesraa@gmail.com>
2025-02-27 15:35:38 +0200

    Add config helpers crate under roles utils
    This crate is intented to host all the shared code that is used across
    the different configs including structs, parsing and other stuff.
    
    This commit also adds `coinbase_output.rs` and `toml.rs` modules to the
    new crate. `coinbase_output.rs` exposes `CoinbaseOutput`, a struct used
    at least by JDS and C configs. `toml.rs` exposes `duration_from_toml`, a
    util used at least by JDS and C configs.

---
+++ roles/roles-utils/config-helpers/src/coinbase_output.rs
@@ -0,0 +1,31 @@
+use roles_logic_sv2::utils::CoinbaseOutput as CoinbaseOutput_;
+use std::convert::TryFrom;
+
+#[derive(Debug, serde::Deserialize, Clone)]
+pub struct CoinbaseOutput {
+    output_script_type: String,
+    output_script_value: String,
+}
+
+impl CoinbaseOutput {
+    pub fn new(output_script_type: String, output_script_value: String) -> Self {
+        Self {
+            output_script_type,
+            output_script_value,
+        }
+    }
+}
+
+impl TryFrom<&CoinbaseOutput> for CoinbaseOutput_ {
+    type Error = roles_logic_sv2::Error;
+
+    fn try_from(pool_output: &CoinbaseOutput) -> Result<Self, Self::Error> {
+        match pool_output.output_script_type.as_str() {
+            "P2PK" | "P2PKH" | "P2WPKH" | "P2SH" | "P2WSH" | "P2TR" => Ok(CoinbaseOutput_ {
+                output_script_type: pool_output.clone().output_script_type,
+                output_script_value: pool_output.clone().output_script_value,
+            }),
+            _ => Err(roles_logic_sv2::Error::UnknownOutputScriptType),
+        }
+    }
+}
