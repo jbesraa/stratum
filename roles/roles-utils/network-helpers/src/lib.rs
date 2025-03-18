ba54851609acaea978f3546ed54e8e6fc5e3d058
jbesraa <jbesraa@gmail.com>
2025-03-18 12:42:03 +0200

    Add `ConnectionSV1` module
    This module can be utilized in order to setup a connection either on the
    upstream or downstream side in a Stratum V1 enviornment.

--- roles/roles-utils/network-helpers/src/lib.rs
+++ roles/roles-utils/network-helpers/src/lib.rs
@@ -1,6 +1,10 @@
 use binary_sv2::{Deserialize, GetSize, Serialize};
 pub mod noise_connection;
 pub mod plain_connection;
+#[cfg(feature = "sv1")]
+pub mod sv1_connection;
+#[cfg(feature = "sv1")]
+pub use serde_json;
 
 use async_channel::{Receiver, RecvError, SendError, Sender};
 use codec_sv2::{Error as CodecError, HandShakeFrame, HandshakeRole, StandardEitherFrame};
