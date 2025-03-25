b21f71b355b30356a15594f6e16bc4a078b168f1
jbesraa <jbesraa@gmail.com>
2025-03-25 17:16:49 +0200

    Drop `AbortHandle` from `Connection::new`
    As those handles are mostly not used and they might cause ghost
    processes because tokio would not drop unneeded tasks if the handle is
    still in scope/explicitly returned.

--- roles/translator/src/lib/upstream_sv2/upstream.rs
+++ roles/translator/src/lib/upstream_sv2/upstream.rs
@@ -154,7 +154,7 @@ impl Upstream {
         );
 
         // Channel to send and receive messages to the SV2 Upstream role
-        let (receiver, sender, _, _) = Connection::new(socket, HandshakeRole::Initiator(initiator))
+        let (receiver, sender) = Connection::new(socket, HandshakeRole::Initiator(initiator))
             .await
             .unwrap();
         // Initialize `UpstreamConnection` with channel for SV2 Upstream role communication and
