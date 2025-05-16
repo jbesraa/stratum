b6721ec9ed7e7c3f9fa38597b8eafead9ec6d4eb
jbesraa <jbesraa@gmail.com>
2025-05-16 12:50:54 +0300

    Add `MockUpstream` module

--- test/integration-tests/lib/mock_downstream.rs
+++ test/integration-tests/lib/mock_downstream.rs
@@ -1,9 +1,10 @@
 use crate::{
     message_aggregator::MessagesAggregator,
     types::{MessageFrame, MsgType},
-    utils::{create_upstream, message_from_frame},
+    utils::{create_downstream, create_upstream, message_from_frame, wait_for_client},
 };
 use async_channel::Sender;
+use codec_sv2::{StandardEitherFrame, Sv2Frame};
 use roles_logic_sv2::parsers::AnyMessage;
 use std::net::SocketAddr;
 use tokio::net::TcpStream;
@@ -42,6 +43,60 @@ impl MockDownstream {
 
     pub fn next_message_from_upstream(&self) -> Option<(MsgType, AnyMessage<'static>)> {
         self.messages_from_upstream.next_message()
+    }
+}
+
+pub struct MockUpstream {
+    listening_address: SocketAddr,
+    messages_from_dowsntream: MessagesAggregator,
+    // This holds the list of tuples. Each tuple refers to message received and what response
+    // should the upstream send back.
+    response_messages: Vec<(MsgType, AnyMessage<'static>)>,
+}
+
+impl MockUpstream {
+    pub fn new(
+        listening_address: SocketAddr,
+        response_messages: Vec<(MsgType, AnyMessage<'static>)>,
+    ) -> Self {
+        Self {
+            listening_address,
+            messages_from_dowsntream: MessagesAggregator::new(),
+            response_messages,
+        }
+    }
+
+    pub async fn start(&self) -> Sender<MessageFrame> {
+        let listening_address = self.listening_address;
+        let (upstream_receiver, upstream_sender) = // FIX NAMING
+            create_downstream(wait_for_client(listening_address).await)
+                .await
+                .expect("Failed to create upstream");
+        let messages_from_dowsntream = self.messages_from_dowsntream.clone();
+        let response_messages = self.response_messages.clone();
+        let sender = upstream_sender.clone();
+        tokio::spawn(async move {
+            while let Ok(mut frame) = upstream_receiver.recv().await {
+                let (msg_type, msg) = message_from_frame(&mut frame);
+                messages_from_dowsntream.add_message(msg_type, msg);
+                // loop through the response messages and send them back to the client
+                let response = response_messages
+                    .iter()
+                    .find(|(msg_type, _)| msg_type == msg_type);
+                if let Some((_, response_msg)) = response {
+                    let message = StandardEitherFrame::<AnyMessage<'_>>::Sv2(
+                        Sv2Frame::from_message(response_msg.clone(), msg_type, 0, false)
+                            .expect("Failed to create the frame"),
+                    );
+                    sender.send(message).await.unwrap();
+                }
+            }
+        });
+        upstream_sender
+    }
+
+    pub fn next_message_from_downstream(&self) -> Option<(MsgType, AnyMessage<'static>)> {
+        self.messages_from_dowsntream.next_message()
     }
 }
 
@@ -84,4 +139,7 @@ mod tests {
             .messages_from_upstream
             .has_message_type(MESSAGE_TYPE_SETUP_CONNECTION_SUCCESS);
     }
+
+    #[tokio::test]
+    async fn test_upstream_mock() {}
 }
